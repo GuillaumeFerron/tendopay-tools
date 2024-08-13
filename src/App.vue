1ea22beaa969feab3e92e8fc9042c7fca45fe233
Guillaume Ferron <guillaume.ferron2@gmail.com>
2024-08-13 16:32:20 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main id="main" class="d-flex align-items-center justify-content-center h-full py-3">
+    <ToolBody />
+  </main>
+</template>
+
+<script setup>
+import ToolBody from './components/common/ToolBody.vue'
+</script>
+
+<style lang="scss">
+@import './scss/index.scss';
+
+#main {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background: url('./assets/bg.jpg') center;
+  background-size: cover;
+  min-height: 100vh;
+}
+</style>
---
+++ src/components/common/ToolBody.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card mb-0 w-75">
+    <div class="card-body px-5">
+      <h3>Title</h3>
+      -- Tool here --
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+ .card {
+    -webkit-box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.3);
+    box-shadow: 5px 5px 15px 5px rgba(0, 0, 0, 0.3);
+  }
+</style>
